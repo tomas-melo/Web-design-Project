0814573c08ee5528ba834d00f1928eeeeb2abb19
unknown <vincent.rodrigues92@gmail.com>
2023-11-22 23:54:28 +0100

    New navbar + Landing Page + New footer

--- script/ScrollIndicator.js
+++ script/ScrollIndicator.js
@@ -1,24 +1,6 @@
-function startHome() {
-    const splashScreen = document.getElementById('splashScreen');
-    const homeContent = document.getElementById('homeContent');
-    const body = document.body;
-  
-    // Désactiver le scroll
-    body.style.overflow = '';
-  
-    splashScreen.classList.add('slide-down');
-    homeContent.classList.add('visible');
-  
-    // Réactiver le scroll après un certain délai (1 seconde dans cet exemple)
-    setTimeout(() => {
-      body.style.overflow = ''; // Réinitialiser la propriété overflow
-    }, 1);
-  }
-
-// Script for scroll indicator
-// Attach the moveProgressBar function to the window's scroll event
+// Script for the progression bar
 window.onscroll = function () {
-        moveProgressBar();
+    moveProgressBar();
 };
 
 // Function to move the progress bar based on scroll position
