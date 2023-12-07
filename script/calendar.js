db090b1d18e599430f520728554bdf9c0cd6ab8e
Lykain17 <kilyanben@gmail.com>
2023-12-07 16:36:05 +0100

    Add new events / Add Comments for all the files Add Button for calendar

--- script/calendar.js
+++ script/calendar.js
@@ -1,30 +1,34 @@
+// Event listener for DOM content loaded
 document.addEventListener('DOMContentLoaded', function() {
     generateCalendar();
     addEventsToCalendar();
 });
 
-function addEventToStorage(title, date, image) {
-    console.log('Ajout des événements au storage');
+// Function to add an event to local storage
+function addEventToStorage(title, date, image, url) {
+    console.log('Adding events to storage');
     var events = JSON.parse(localStorage.getItem('events') || '[]');
-    var eventExists = events.some(event => event.title === title && event.date === date && event.image == image);
-    console.log("Événements existants:", events);
+    var eventExists = events.some(event => event.title === title && event.date === date && event.image == image && event.url == url);
+    console.log("Existing events:", events);
     if (!eventExists) {
-        console.log("image");
-        events.push({ title: title, date: date, image: image });
+        console.log("Adding new event");
+        events.push({ title: title, date: date, image: image, url:url});
         localStorage.setItem('events', JSON.stringify(events));
     }
-  }
+}
+
+// Function to generate the calendar
 function generateCalendar() {
     const calendar = document.getElementById('calendar');
-    const month = 11; // Décembre (Janvier est 0)
+    const month = 11; // December (January is 0)
     const year = new Date().getFullYear();
     const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-    let table = `<table><tr><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thrusday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr><tr>`;
+    let table = `<table><tr><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr><tr>`;
 
     for (let i = 1; i <= daysInMonth; i++) {
         let day = new Date(year, month, i).getDay();
-        day = day === 0 ? 7 : day; // Ajuster le dimanche de 0 à 7
+        day = day === 0 ? 7 : day; // Adjust Sunday from 0 to 7
 
         if (i === 1 && day !== 1) {
             table += `<td colspan="${day - 1}"></td>`;
@@ -41,26 +45,20 @@ function generateCalendar() {
     calendar.innerHTML = table;
 }
 
+// Function to add events to the calendar
 function addEventsToCalendar() {
-    console.log('Ajout des événements au calendrier');
     const events = JSON.parse(localStorage.getItem('events') || '[]');
-    
-    const calendarCells = document.querySelectorAll('#calendar td');
-    calendarCells.forEach(cell => {
-        const date = cell.getAttribute('data-date');
-        if (date) {
-            cell.innerHTML = `<div class="date">${cell.innerText}</div>`; // Assurez-vous de conserver la date dans la cellule
-        }
-    });
+
     events.forEach(function(event) {
         const calendarCell = document.querySelector(`#calendar td[data-date="${event.date}"]`);
         if (calendarCell) {
             const eventContent = `
-                <div class="calendar-event">
-                    <img src="${event.image}" alt="${event.title}" style="width:100%; height:auto;">
-                    <span>${event.title}</span>
-                </div>`;
-            // Ajouter le nouveau contenu
+                <a href="${event.url}" class="calendar-event-link" target="_blank">
+                    <div class="calendar-event">
+                        <img src="${event.image}" alt="${event.title}" style="width:100%; height:auto;">
+                        <span>${event.title}</span>
+                    </div>
+                </a>`;
             calendarCell.innerHTML = eventContent;
         }
     });
--- script/services.js
+++ script/services.js
@@ -1,11 +1,11 @@
 var map;
 var infowindows = [];
-var city = {lat: 41.1496100, lng: -8.6109900}; // Coordonnées de Porto, Portugal
-var compteur = 0;
+var city = {lat: 41.1496100, lng: -8.6109900}; // Coordinates of Porto, Portugal
+var counter = 0;
 var currentType = 'all';
 
+// Initialize the map
 function initMap() {
-
     clearMarkers();
 
     map = new google.maps.Map(document.getElementById('map'), {
@@ -16,19 +16,20 @@ function initMap() {
     performSearch('restaurant');
 }
 
+// Perform a search for places
 function performSearch(type) {
-        var request = {
-            location: city,
-            radius: '10000',
-            type: [type]
-        };
+    var request = {
+        location: city,
+        radius: '10000',
+        type: [type]
+    };
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, function(results, status, pagination) {
-    console.log(service)
+    console.log(service);
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         createMarkers(results, type);
 
-        // Si d'autres pages sont disponibles, attendez un peu avant de les charger
+        // If more pages are available, wait a bit before loading them
         if (pagination.hasNextPage) {
             setTimeout(function() {
                 pagination.nextPage();
@@ -38,20 +39,21 @@ function performSearch(type) {
 });
 }
 
+// Function to create a marker
 function createMarker(place, type) {
     var marker = new google.maps.Marker({
         position: place.geometry.location,
         map: map,
         title: place.name
     });
-    compteur+=1;
-    console.log(compteur);
+    counter++;
+    console.log(counter);
     var typeLabel = type === 'bar' ? 'Bar' : 'Restaurant';
-    var rating = place.rating ? place.rating + ' ' + getStars(place.rating) : 'Pas de note';
+    var rating = place.rating ? place.rating + ' ' + getStars(place.rating) : 'No rating';
     var contentString = '<div class="info-window">' +
         '<h3>' + place.name + '</h3>' + '<p>Type: ' + typeLabel + '</p>' + 
-        '<p>Note: ' + rating + '</p>' +
-        (place.website ? '<p><a href="' + place.website + '" target="_blank">Site web</a></p>' : '') +
+        '<p>Rating: ' + rating + '</p>' +
+        (place.website ? '<p><a href="' + place.website + '" target="_blank">Website</a></p>' : '') +
         '</div>';
 
     var infowindow = new google.maps.InfoWindow({
@@ -59,7 +61,7 @@ function createMarker(place, type) {
     });
 
     marker.addListener('click', function() {
-        infowindows.forEach(function(item) { item.infowindow.close(); }); // Ferme toutes les infobulles ouvertes
+        infowindows.forEach(function(item) { item.infowindow.close(); }); // Close all open infowindows
         infowindow.open(map, marker);
     });
 
@@ -67,18 +69,20 @@ function createMarker(place, type) {
     infowindows.push({infowindow, marker, type});
 }
 
+// Function to get star ratings
 function getStars(rating) {
     var stars = '';
     for (var i = 0; i < 5; i++) {
         if (rating >= i + 0.5) {
-            stars += '\u2605'; // Unicode étoile pleine
+            stars += '\u2605'; // Unicode for full star
         } else {
-            stars += '\u2606'; // Unicode étoile vide
+            stars += '\u2606'; // Unicode for empty star
         }
     }
     return stars;
 }
 
+// Toggle visibility of infowindows
 function toggleInfowindows(show) {
     infowindows.forEach(function(item) {
         if(((currentType === 'all' || item.type === currentType) && show)) {
@@ -89,16 +93,19 @@ function toggleInfowindows(show) {
     });
 }
 
+// Function to show places of a specific type
 function showPlaces(typeToShow) {
     currentType = typeToShow;
     infowindows.forEach(function(item) {
         if (item.type === typeToShow || typeToShow === 'all') {
-            item.marker.setMap(map); // Afficher le marqueur
+            item.marker.setMap(map); // Show the marker
         } else {
-            item.marker.setMap(null); // Masquer le marqueur
+            item.marker.setMap(null); // Hide the marker
         }
     });
 }
+
+// Function to create markers for results
 function createMarkers(results, type) {
     for (var i = 0; i < results.length; i++) {
         if (!results[i].types.includes('lodging') && results[i].rating >= 4.5) {
@@ -107,17 +114,41 @@ function createMarkers(results, type) {
     }
 }
 
+// Function to clear all markers
 function clearMarkers() {
     for (var i = 0; i < infowindows.length; i++) {
         infowindows[i].marker.setMap(null);
     }
-    infowindows = []; // Réinitialisez également le tableau des infowindows
+    infowindows = []; // Also reset the infowindows array
 }
 
+// Event listeners for buttons
 document.getElementById('showBars').addEventListener('click', function() {
     showPlaces('bar');
+    selectButton('showBars', false);
 });
 
 document.getElementById('showRestaurants').addEventListener('click', function() {
     showPlaces('restaurant');
+    selectButton('showRestaurants', false);
 });
+
+// Function to select a button
+function selectButton(buttonId, isInfoButton) {
+    var button = document.getElementById(buttonId);
+    if (button) {
+        if (isInfoButton) {
+            // For info buttons, make sure only one can be selected at a time
+            var otherButtonId = buttonId === 'showInfo' ? 'hideInfo' : 'showInfo';
+            var otherButton = document.getElementById(otherButtonId);
+            button.classList.add('selected');
+            otherButton.classList.remove('selected');
+        } else {
+            // For type of place buttons, normal behavior
+            document.querySelectorAll('.button a:not(.info-button)').forEach(function(btn) {
+                btn.classList.remove('selected');
+            });
+            button.classList.add('selected');
+        }
+    }
+}
